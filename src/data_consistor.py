38810a5bd0b48736791cb016fce0ab3ed2320ccb
MrNemesis98 <84439444+MrNemesis98@users.noreply.github.com>
2023-12-22 15:41:54 +0100

    progress

---
+++ src/data_consistor.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import regex as re
+
+
+def scan_for_errors(file, make_corrections=False):
+    df = pd.read_csv(file)
+    counts = 0
+    for index, row in df.iterrows():
+        current_span = str(row['span'])
+
+        # Regulärer Ausdruck, um die erste Zahl zu extrahieren
+        pattern = r'\[([0-9]+),\s*[0-9]+\]'
+        match = re.match(pattern, current_span)
+        current_row = index + 2
+        if not match and current_row != 2:
+            print("Span error in Zeile {}: {}".format(current_row, current_span))
+            if make_corrections and current_row != 2:
+                print(current_row)
+                df['span'].replace(current_span, pd.NA, inplace=True)
+                df_cleaned = df.dropna(subset=['span'])
+                df_cleaned.to_csv(file, index=False)
+            counts += 1
+            print(counts)
+
+
+scan_for_errors("corona/preprocessed/non_cpd/prosem_data_corpus-corona_term-warum_processed.csv",
+                make_corrections=False)
+
